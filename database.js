2965611b97451313e4cb5acf777ca8190ac3d180
raflyfachrezim-png <raflyfachrezim@gmail.com>
2026-07-06 02:11:50 +0700

    Refactor database structure for agricultural data

--- database.js
+++ database.js
@@ -1,12 +1,18 @@
-const database={
-Pertanian:{
-Padi:{patokan:1000,satuan:"m²",upah:840000,produksi:700000,barang:0,operasional:200000,non:0,pendapatan:3000000},
-Sawi:{patokan:1000,satuan:"m²",upah:840000,produksi:900000,barang:0,operasional:450000,non:0,pendapatan:2700000},
-Timun:{patokan:1000,satuan:"m²",upah:3000000,produksi:7060000,barang:0,operasional:1500000,non:0,pendapatan:12000000},
-Cabe:{patokan:1000,satuan:"m²",upah:3000000,produksi:23000000,barang:0,operasional:5000000,non:0,pendapatan:37500000}},
-Perdagangan:{
-"Warung Eceran":{patokan:100000,satuan:"Rupiah",upah:0,produksi:0,barang:30600000,operasional:1800000,non:0,pendapatan:36000000},
-"Perdagangan Ikan":{patokan:1,satuan:"KG",upah:0,produksi:30000,barang:0,operasional:2000,non:0,pendapatan:35000}},
-Kuliner:{"Warung Makan/Minum":{patokan:100000,satuan:"Rupiah",upah:0,produksi:18000000,barang:0,operasional:5400000,non:0,pendapatan:36000000}},
-Peternakan:{"Domba Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:900000,barang:0,operasional:365000,non:0,pendapatan:2500000},"Domba Pembibitan":{patokan:1,satuan:"Ekor",upah:0,produksi:0,barang:0,operasional:365000,non:0,pendapatan:2500000},"Sapi Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:15000000,barang:0,operasional:4800000,non:0,pendapatan:25000000}}
+const dataPatokan = {
+  pertanian: {
+    padi: { nama: "Padi", upah: 2520000, produksi: 2100000, operasional: 600000, nonop: 0, pendapatan: 9000000, panen_per_tahun: 3 },
+    sawi: { nama: "Sawi", upah: 10080000, produksi: 10800000, operasional: 5400000, nonop: 0, pendapatan: 32400000, panen_per_tahun: 12 },
+    timun: { nama: "Timun", upah: 30000000, produksi: 70600000, operasional: 15000000, nonop: 0, pendapatan: 120000000, panen_per_tahun: 10 },
+    cabe: { nama: "Cabe", upah: 6000000, produksi: 46000000, operasional: 10000000, nonop: 0, pendapatan: 75000000, panen_per_tahun: 2 }
+  },
+  perdagangan: {
+    warung_eceran: { nama: "Warung Eceran", upah: 0, produksi: 0, hpp: 30600000, operasional: 1800000, nonop: 0, pendapatan: 36000000 },
+    warung_makan: { nama: "Warung Makan/Minum", upah: 0, produksi: 18000000, hpp: 0, operasional: 5400000, nonop: 0, pendapatan: 36000000 },
+    dagang_ikan: { nama: "Perdagangan Ikan", upah: 0, produksi: 30000, hpp: 0, operasional: 2000, nonop: 0, pendapatan: 35000 } // Patokan per 1 KG, dihitung proporsional harian
+  },
+  peternakan: {
+    domba_penggemukan: { nama: "Domba Penggemukan", upah: 0, produksi: 900000, hpp: 0, operasional: 365000, nonop: 0, pendapatan: 2500000 },
+    domba_pembibitan: { nama: "Domba Pembibitan", upah: 0, produksi: 0, hpp: 365000, operasional: 0, nonop: 0, pendapatan: 2500000 },
+    sapi_penggemukan: { nama: "Sapi Penggemukan", upah: 0, produksi: 15000000, hpp: 0, operasional: 4800000, nonop: 0, pendapatan: 25000000 }
+  }
 };
