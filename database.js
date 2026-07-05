c47fd65f0a6e9a61d0178a04ad690b4545721352
raflyfachrezim-png <raflyfachrezim@gmail.com>
2026-07-06 01:27:47 +0700

    Add new agricultural and trade data to database

--- database.js
+++ database.js
@@ -1,8 +1,12 @@
 const database={
+Pertanian:{
+Padi:{patokan:1000,satuan:"m²",upah:840000,produksi:700000,barang:0,operasional:200000,non:0,pendapatan:3000000},
+Sawi:{patokan:1000,satuan:"m²",upah:840000,produksi:900000,barang:0,operasional:450000,non:0,pendapatan:2700000},
+Timun:{patokan:1000,satuan:"m²",upah:3000000,produksi:7060000,barang:0,operasional:1500000,non:0,pendapatan:12000000},
+Cabe:{patokan:1000,satuan:"m²",upah:3000000,produksi:23000000,barang:0,operasional:5000000,non:0,pendapatan:37500000}},
+Perdagangan:{
 "Warung Eceran":{patokan:100000,satuan:"Rupiah",upah:0,produksi:0,barang:30600000,operasional:1800000,non:0,pendapatan:36000000},
-"Warung Makan/Minum":{patokan:100000,satuan:"Rupiah",upah:0,produksi:18000000,barang:0,operasional:5400000,non:0,pendapatan:36000000},
-"Domba Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:900000,barang:0,operasional:365000,non:0,pendapatan:2500000},
-"Domba Pembibitan":{patokan:1,satuan:"Ekor",upah:0,produksi:0,barang:0,operasional:365000,non:0,pendapatan:2500000},
-"Sapi Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:15000000,barang:0,operasional:4800000,non:0,pendapatan:25000000},
-"Perdagangan Ikan":{patokan:1,satuan:"KG",upah:0,produksi:30000,barang:0,operasional:2000,non:0,pendapatan:35000}
+"Perdagangan Ikan":{patokan:1,satuan:"KG",upah:0,produksi:30000,barang:0,operasional:2000,non:0,pendapatan:35000}},
+Kuliner:{"Warung Makan/Minum":{patokan:100000,satuan:"Rupiah",upah:0,produksi:18000000,barang:0,operasional:5400000,non:0,pendapatan:36000000}},
+Peternakan:{"Domba Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:900000,barang:0,operasional:365000,non:0,pendapatan:2500000},"Domba Pembibitan":{patokan:1,satuan:"Ekor",upah:0,produksi:0,barang:0,operasional:365000,non:0,pendapatan:2500000},"Sapi Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:15000000,barang:0,operasional:4800000,non:0,pendapatan:25000000}}
 };
