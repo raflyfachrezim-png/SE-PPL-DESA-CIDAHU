97c241c408ddb7d185d8ca31334b7449e922df44
raflyfachrezim-png <raflyfachrezim@gmail.com>
2026-07-06 01:19:27 +0700

    Add initial database structure for various entities

---
+++ database.js
@@ -0,0 +1,8 @@
+const database={
+"Warung Eceran":{patokan:100000,satuan:"Rupiah",upah:0,produksi:0,barang:30600000,operasional:1800000,non:0,pendapatan:36000000},
+"Warung Makan/Minum":{patokan:100000,satuan:"Rupiah",upah:0,produksi:18000000,barang:0,operasional:5400000,non:0,pendapatan:36000000},
+"Domba Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:900000,barang:0,operasional:365000,non:0,pendapatan:2500000},
+"Domba Pembibitan":{patokan:1,satuan:"Ekor",upah:0,produksi:0,barang:0,operasional:365000,non:0,pendapatan:2500000},
+"Sapi Penggemukan":{patokan:1,satuan:"Ekor",upah:0,produksi:15000000,barang:0,operasional:4800000,non:0,pendapatan:25000000},
+"Perdagangan Ikan":{patokan:1,satuan:"KG",upah:0,produksi:30000,barang:0,operasional:2000,non:0,pendapatan:35000}
+};
